b15ba14b2160747e5656c53245717aee05d74fd1
Marcin Wąsik <marcinmw99@gmail.com>
2018-02-03 18:59:46 +0100

    Fix not loading jquery

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,26 @@
-var url = 'https://restcountries.eu/rest/v1/name/';
-var countriesList = $('#countries');
+$(function () {
+	var url = 'https://restcountries.eu/rest/v1/name/';
+	var countriesList = $('#countries');
 
-$('#search').click(searchCountries);
+	$('#search').click(searchCountries);
 
-function searchCountries() {
-	var countryName = $('#country-name').val();
-	if(!countryName.length) { 
-		countryName = 'Poland'; 
+	function searchCountries() {
+		var countryName = $('#country-name').val();
+		if(!countryName.length) { 
+			countryName = 'Poland'; 
+		}
+		$.ajax({
+			url: url + countryName,
+			method: 'GET',
+			success: showCountriesList
+		});
 	}
-	$.ajax({
-		url: url + countryName,
-		method: 'GET',
-		success: showCountriesList
-	});
-}
 
-function showCountriesList(response) {
-	countriesList.empty();
-}
+	function showCountriesList(response) {
+		countriesList.empty();
+		response.forEach(function(item) {
+		$("<li>").text(item.name).appendTo(countriesList);
+		});
+	}
+
+});
